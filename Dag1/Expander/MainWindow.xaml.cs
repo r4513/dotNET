5d6ce132512d84fbcb73af4c5ded4cc69dc1f5a2
r4513 <rasmuschristensen87@gmail.com>
2016-02-02 18:28:07 +0100

    Opgave 3 del 1

---
+++ Dag1/Expander/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Expander
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void onAddButtonAction(object sender, RoutedEventArgs e)
+        {
+            Service.list.Add(new Person
+            {
+                Cpr = "000000",
+                FirstName = nameTextBox.Text,
+                LastName = "",
+                YearOfBirth = -1
+            });
+        }
+
+        private void Start(object sender, RoutedEventArgs e)
+        {
+            lBoxPersons.ItemsSource = Service.list;
+            lBoxPersons.DisplayMemberPath = "Cpr";
+        }
+
+        private void lBoxPersonsSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Person p = (Person)lBoxPersons.SelectedItem;
+            nameTextBox.Text = p.FirstName;
+        }
+
+        private void onCloseBtnAction(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Sure you want to exit this awesome game?", "Stopping", MessageBoxButton.YesNo);
+            if (result == MessageBoxResult.No)
+            {
+                return;
+            }
+
+        }
+
+        private void closingEvent(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Sure you want to exit this awesome game?", "Stopping", MessageBoxButton.YesNo);
+            if (result == MessageBoxResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
+    }
+}
---
+++ Dag1/Expander/Model.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expander
+{
+    class Person
+    {
+        string cpr;
+        public string Cpr
+        {
+            get
+            {
+                return cpr;
+            }
+            set
+            {
+                cpr = value;
+            }
+        }
+        string firstName;
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+            set
+            {
+                firstName = value;
+            }
+        }
+        string lastName;
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+            set
+            {
+                lastName = value;
+            }
+        }
+        int yearOfBirth;
+        public int YearOfBirth
+        {
+            get
+            {
+                return yearOfBirth;
+            }
+            set
+            {
+                yearOfBirth = value;
+            }
+        }
+    }
+
+    class Service
+    {
+        public static ObservableCollection<Person> list = new ObservableCollection<Person>();
+        static Service()
+        {
+            list.Add(new Person
+            {
+                Cpr = "12345",
+                FirstName = "Bill",
+                LastName = "Smith",
+                YearOfBirth = 1980
+            });
+            list.Add(new Person
+            {
+                Cpr = "23456",
+                FirstName = "Chris",
+                LastName = "Larsson",
+                YearOfBirth = 1988
+            });
+            list.Add(new Person
+            {
+                Cpr = "34567",
+                FirstName = "Susan",
+                LastName = "Ford",
+                YearOfBirth = 1995
+            });
+            list.Add(new Person
+            {
+                Cpr = "45678",
+                FirstName = "Jane",
+                LastName = "Doe",
+                YearOfBirth = 1977
+            });
+            list.Add(new Person
+            {
+                Cpr = "56789",
+                FirstName = "Bill",
+                LastName = "Doe",
+                YearOfBirth = 1986
+            });
+        }
+    }
+}
