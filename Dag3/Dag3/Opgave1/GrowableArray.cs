6c42c331ed52f671fd7264f3e22a1d04a34e54e8
Rasmus <rasmuschristensen87@gmail.com>
2016-02-12 10:47:30 +0100

    opgave1 dag 3
    Yiha

---
+++ Dag3/Dag3/Opgave1/GrowableArray.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opgave1
+{
+    class GrowableArray
+    {
+        static void Main(string[] args)
+        {
+            GrowableArray g = new GrowableArray();
+            g[0] = 7;
+            g[1] = 9;
+            g[2] = 13;
+            Console.WriteLine(g[2]);
+            Console.ReadLine();
+        }
+
+        public GrowableArray()
+        {
+            this.arr = new int[16];
+        }
+
+        public int[] arr;
+
+        public int this[int i]
+        {
+            get
+            {
+                if (i > arr.Length - 1 || i < 0)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+                else
+                {
+                    return arr[i];
+                }
+            }
+
+            set
+            {
+                if (i < 0)
+                {
+                    throw new IndexOutOfRangeException();
+                } else if (i > arr.Length - 1) {
+                    int[] temp = new int[this.arr.Length * 2];
+                    Array.Copy(this.arr, temp, this.arr.Length);
+                    this.arr = temp;
+                }
+                else
+                {
+                    arr[i] = value;
+                }
+            }
+        }
+    }
+}
---
+++ indexer/indexer/indexer/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace indexer
+{
+
+    public struct Vector
+    {
+        private double x, y, z;
+
+        public Vector(int x, int y, int z)
+        {
+            this.x = x; this.y = y; this.z = z;
+        }
+
+        public double X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+
+        public double this[int i]
+        {
+            get
+            {
+                switch (i)
+                {
+                    case 1: return x;
+                    case 2: return y;
+                    case 3: return z;
+                    default:
+                        throw new IndexOutOfRangeException("get Vector " + i);
+                }
+            }
+
+            set
+            {
+                switch (i)
+                {
+                    case 1: x = value; break;
+                    case 2: y = value; break;
+                    case 3: z = value; break;
+                    default:
+                        throw new IndexOutOfRangeException("set Vector " + i);
+                }
+            }
+        }
+
+        public double this[char i]
+        {
+            get
+            {
+                switch (i)
+                {
+                    case 'x': return x;
+                    case 'y': return y;
+                    case 'z': return z;
+                    default:
+                        throw new IndexOutOfRangeException("get Vector " + i);
+                }
+            }
+
+            set
+            {
+                switch (i)
+                {
+                    case 'x': x = value; break;
+                    case 'y': y = value; break;
+                    case 'z': z = value; break;
+                    default:
+                        throw new IndexOutOfRangeException("set Vector " + i);
+                }
+            }
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector v = new Vector(7, 9, 13);
+
+            Console.WriteLine(v[1] + " " + v[2] + " " + v[3]);
+            v[2] = 100;
+            Console.WriteLine(v[1] + " " + v[2] + " " + v[3]);
+            v['z'] = 200;
+            Console.WriteLine(v['x'] + " " + v['y'] + " " + v['z']);
+
+            Console.ReadLine();
+        }
+    }
+
+}
